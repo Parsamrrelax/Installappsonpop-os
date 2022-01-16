101a6eadd9a29bc704aad930b5c2ac827c87e4a9
Parsamrrelax <40731455+Parsamrrelax@users.noreply.github.com>
2022-01-16 15:10:07 +0330

    Added neccesary files
    added some apps
    outline
    fonts

--- install.sh
+++ install.sh
@@ -12,3 +12,18 @@ echo "MPV INSTALLED"
 sudo apt install --yes flameshot
 echo "FLAMESHOT INSTALLED"
 
+sudo apt install --yes gimp
+echo "GIMP INSTALLED"
+
+sudo apt install --yes telegram-desktop
+echo "TELEGRAM INSTALLED"
+
+
+mkdir /home/parsa/.fonts
+cp '/home/parsa/Installappsonpop-os/Downloading files/MesloLGS NF Bold Italic.ttf' '/home/parsa/Installappsonpop-os/Downloading files/MesloLGS NF Bold.ttf' '/home/parsa/Installappsonpop-os/Downloading files/MesloLGS NF Italic.ttf' '/home/parsa/Installappsonpop-os/Downloading files/MesloLGS NF Regular.ttf' /home/parsa/.fonts/
+fc-cache -fv
+echo "NEW FONTS ADDED"
+
+chmox +x Outline-Client.Appimage
+echo "OUTLINE INSTALLED"
+
