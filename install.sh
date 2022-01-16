3da27142a9431a6b92a9ec1cc20c60d1d7718899
Parsamrrelax <40731455+Parsamrrelax@users.noreply.github.com>
2022-01-16 15:11:58 +0330

    Update install.sh

--- install.sh
+++ install.sh
@@ -5,7 +5,7 @@ echo "UPDATE COMPLETE"
 # sudo apt upgrade
 # echo "UPGRADE COMPLETE"
 
-
+#Apps Installing
 sudo apt-get --yes install mpv
 echo "MPV INSTALLED"
 
@@ -18,7 +18,7 @@ echo "GIMP INSTALLED"
 sudo apt install --yes telegram-desktop
 echo "TELEGRAM INSTALLED"
 
-
+#System edits
 mkdir /home/parsa/.fonts
 cp '/home/parsa/Installappsonpop-os/Downloading files/MesloLGS NF Bold Italic.ttf' '/home/parsa/Installappsonpop-os/Downloading files/MesloLGS NF Bold.ttf' '/home/parsa/Installappsonpop-os/Downloading files/MesloLGS NF Italic.ttf' '/home/parsa/Installappsonpop-os/Downloading files/MesloLGS NF Regular.ttf' /home/parsa/.fonts/
 fc-cache -fv
